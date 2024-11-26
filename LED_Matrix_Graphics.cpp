3f722e60b961c01728d59b17d5061ffa1346f27e
msharipov <msharipovr@gmail.com>
2024-11-26 16:32:26 -0500

    style: move font declarations to the bottom

--- LED_Matrix_Graphics.cpp
+++ LED_Matrix_Graphics.cpp
@@ -3,6 +3,163 @@
 
 namespace LMG {
 
+void set_bit(uint32_t f[], const int8_t row, const int8_t col, const bool bit) {
+
+  uint8_t q, r, pos;
+
+  pos = row * 12 + col;
+
+  if (pos < 32) {
+    q = 2;
+  } else if (pos < 64) {
+    q = 1;
+  } else {
+    q = 0;
+  }
+  r = pos % 32;
+
+  if (bit) {
+    f[q] |= (1UL << r);
+  } else {
+    f[q] &= ~(1UL << r);
+  }
+}
+
+void invert_bit(uint32_t f[], const int8_t row, const int8_t col) {
+
+  uint8_t q, r, pos;
+
+  pos = row * 12 + col;
+
+  if (pos < 32) {
+    q = 2;
+  } else if (pos < 64) {
+    q = 1;
+  } else {
+    q = 0;
+  }
+  r = pos % 32;
+
+  f[q] ^= (1UL << r);
+}
+
+void put_sym(uint32_t f[], const bool *symbol, const int8_t row,
+             const int8_t col, const int8_t width, const int8_t height) {
+
+  for (int8_t add_col = 0; add_col < width; add_col++) {
+    for (int8_t add_row = 0; add_row < height; add_row++) {
+
+      set_bit(f, row + add_row, col + add_col,
+              *(symbol + sizeof(bool) * (add_col + add_row * width)));
+    }
+  }
+}
+
+void put_sym_bnd(uint32_t f[], const bool *symbol, const int8_t row,
+                 const int8_t col, const int8_t width, const int8_t height,
+                 const int8_t row_l, const int8_t col_l, const int8_t row_h,
+                 const int8_t col_h) {
+
+  for (int8_t add_col = 0; add_col < width; add_col++) {
+
+    if (col + add_col > col_h || col + add_col < col_l) {
+      continue;
+    }
+
+    for (int8_t add_row = 0; add_row < height; add_row++) {
+
+      if (row + add_row > row_h || row + add_row < row_l) {
+        continue;
+      }
+
+      set_bit(f, row + add_row, col + add_col,
+              *(symbol + sizeof(bool) * (add_col + add_row * width)));
+    }
+  }
+}
+
+void fill_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
+               const int8_t row_h, const int8_t col_h, const bool bit) {
+
+  for (int8_t col = col_l; col <= col_h; col++) {
+    for (int8_t row = row_l; row <= row_h; row++) {
+      set_bit(f, row, col, bit);
+    }
+  }
+}
+
+void invert_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
+                 const int8_t row_h, const int8_t col_h) {
+
+  for (int8_t col = col_l; col <= col_h; col++) {
+    for (int8_t row = row_l; row <= row_h; row++) {
+      invert_bit(f, row, col);
+    }
+  }
+}
+
+void draw_text_3_4(uint32_t f[], const bool symbols[][12], int8_t col_h,
+                   int8_t col_l, const int8_t row, const uint8_t msg[],
+                   const std::size_t msg_len, const uint8_t spacing,
+                   const int8_t step) {
+
+  col_l = (col_l < 0) ? 0 : col_l;
+  col_h = (col_h > 11) ? 11 : col_h;
+
+  const int8_t block_width = 3 + spacing;
+  int8_t col = col_h - (2 - step);
+
+  // Skip the printing if the text is completely out of bounds.
+  // Warning: Removing the type cast causes incorrect integer promotion!
+  if (step >= (int16_t)(msg_len * block_width - spacing)) {
+    fill_rect(f, row, col_l, row + 3, col_h, 0);
+    return;
+  }
+
+  // Left-side spacing
+  if (step < 0) {
+    fill_rect(f, row, col + 3, row + 3, col_h, 0);
+  }
+
+  for (int8_t sym = 0; sym < msg_len; sym++) {
+
+    if (col > 11) {
+      col -= block_width;
+      continue;
+    }
+
+    if (col < -2) {
+      break;
+    }
+
+    // Tail spacing before first symbol
+    if (spacing != 0 && col_h - col >= 2 && col_h - col < block_width) {
+      fill_rect(f, row, col + 3, row + 3, col_h, 0);
+    }
+
+    put_sym_bnd(f, symbols[msg[sym]], row, col, 3, 4, row, col_l, row + 3,
+                col_h);
+
+    // Tail spacing
+    if (spacing != 0 && col > col_l) {
+
+      int8_t spc_end = col - spacing;
+      if (spc_end < col_l) {
+        spc_end = col_l;
+      }
+
+      fill_rect(f, row, spc_end, row + 3, col - 1, 0);
+    }
+
+    col -= block_width;
+  }
+
+  // Right-side space
+  if (col > col_l) {
+    fill_rect(f, row, col_l, row + 3, col - 1, 0);
+  }
+}
+
 const bool DIGITS_35[10][15] = {{0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0},
                                 {1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0},
                                 {1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1},
@@ -52,163 +209,5 @@ const bool DEFAULT_FONT_3_4[36][12] = {
     {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1}, // 8
     {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1}  // 9
 };
-
-void set_bit(uint32_t f[], const int8_t row, const int8_t col,
-                 const bool bit) {
-
-  uint8_t q, r, pos;
-
-  pos = row * 12 + col;
-
-  if (pos < 32) {
-    q = 2;
-  } else if (pos < 64) {
-    q = 1;
-  } else {
-    q = 0;
-  }
-  r = pos % 32;
-
-  if (bit) {
-    f[q] |= (1UL << r);
-  } else {
-    f[q] &= ~(1UL << r);
-  }
-}
-
-void invert_bit(uint32_t f[], const int8_t row, const int8_t col) {
-
-  uint8_t q, r, pos;
-
-  pos = row * 12 + col;
-
-  if (pos < 32) {
-    q = 2;
-  } else if (pos < 64) {
-    q = 1;
-  } else {
-    q = 0;
-  }
-  r = pos % 32;
-
-  f[q] ^= (1UL << r);
-}
-
-void put_sym(uint32_t f[], const bool *symbol, const int8_t row,
-                 const int8_t col, const int8_t width, const int8_t height) {
-
-  for (int8_t add_col = 0; add_col < width; add_col++) {
-    for (int8_t add_row = 0; add_row < height; add_row++) {
-
-      set_bit(f, row + add_row, col + add_col,
-                  *(symbol + sizeof(bool) * (add_col + add_row * width)));
-    }
-  }
-}
-
-void put_sym_bnd(uint32_t f[], const bool *symbol, const int8_t row,
-                     const int8_t col, const int8_t width, const int8_t height,
-                     const int8_t row_l, const int8_t col_l, const int8_t row_h,
-                     const int8_t col_h) {
-
-  for (int8_t add_col = 0; add_col < width; add_col++) {
-
-    if (col + add_col > col_h || col + add_col < col_l) {
-      continue;
-    }
-
-    for (int8_t add_row = 0; add_row < height; add_row++) {
-
-      if (row + add_row > row_h || row + add_row < row_l) {
-        continue;
-      }
-
-      set_bit(f, row + add_row, col + add_col,
-                  *(symbol + sizeof(bool) * (add_col + add_row * width)));
-    }
-  }
-}
-
-void fill_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
-                   const int8_t row_h, const int8_t col_h, const bool bit) {
-
-  for (int8_t col = col_l; col <= col_h; col++) {
-    for (int8_t row = row_l; row <= row_h; row++) {
-      set_bit(f, row, col, bit);
-    }
-  }
-}
-
-void invert_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
-                     const int8_t row_h, const int8_t col_h) {
-
-  for (int8_t col = col_l; col <= col_h; col++) {
-    for (int8_t row = row_l; row <= row_h; row++) {
-      invert_bit(f, row, col);
-    }
-  }
-}
-
-void draw_text_3_4(uint32_t f[], const bool symbols[][12], int8_t col_h,
-                     int8_t col_l, const int8_t row, const uint8_t msg[],
-                     const std::size_t msg_len, const uint8_t spacing,
-                     const int8_t step) {
-
-  col_l = (col_l < 0) ? 0 : col_l;
-  col_h = (col_h > 11) ? 11 : col_h;
-
-  const int8_t block_width = 3 + spacing;
-  int8_t col = col_h - (2 - step);
-
-  // Skip the printing if the text is completely out of bounds.
-  // Warning: Removing the type cast causes incorrect integer promotion!
-  if (step >= (int16_t)(msg_len * block_width - spacing)) {
-    fill_rect(f, row, col_l, row + 3, col_h, 0);
-    return;
-  }
-
-  // Left-side spacing
-  if (step < 0) {
-    fill_rect(f, row, col + 3, row + 3, col_h, 0);
-  }
-
-  for (int8_t sym = 0; sym < msg_len; sym++) {
-
-    if (col > 11) {
-      col -= block_width;
-      continue;
-    }
-
-    if (col < -2) {
-      break;
-    }
-
-    // Tail spacing before first symbol
-    if (spacing != 0 && col_h - col >= 2 && col_h - col < block_width) {
-      fill_rect(f, row, col + 3, row + 3, col_h, 0);
-    }
-
-    put_sym_bnd(f, symbols[msg[sym]], row, col, 3, 4, row, col_l, row + 3,
-                    col_h);
-
-    // Tail spacing
-    if (spacing != 0 && col > col_l) {
-
-      int8_t spc_end = col - spacing;
-      if (spc_end < col_l) {
-        spc_end = col_l;
-      }
-
-      fill_rect(f, row, spc_end, row + 3, col - 1, 0);
-    }
-
-    col -= block_width;
-  }
-
-  // Right-side space
-  if (col > col_l) {
-    fill_rect(f, row, col_l, row + 3, col - 1, 0);
-  }
-}
 
 } // namespace LMG
--- LED_Matrix_Graphics.h
+++ LED_Matrix_Graphics.h
@@ -5,6 +5,45 @@
 #include <stdint.h>
 
 namespace LMG {
+
+// Sets the LED at ([row], [col]) to the value of bit.
+void set_bit(uint32_t f[], const int8_t row, const int8_t col, const bool bit);
+
+// Inverts the state of the LED at ([row], [col]).
+void invert_bit(uint32_t f[], const int8_t row, const int8_t col);
+
+// Draws a [width]-by-[height] symbol to the LED matrix with its bottom
+// right corner at coordinates given by [row] and [column]. All LEDs
+// used by the symbol must lie within the matrix.
+void put_sym(uint32_t f[], const bool *symbols, const int8_t row,
+             const int8_t col, const int8_t width, const int8_t height);
+
+// Works like put_sym() but skips every position that is outside of the
+// rectangle given by vertices ([row_l], [col_l]) and ([row_h], [col_h]).
+void put_sym_bnd(uint32_t f[], const bool *symbols, const int8_t row,
+                 const int8_t col, const int8_t width, const int8_t height,
+                 const int8_t row_l, const int8_t col_l, const int8_t row_h,
+                 const int8_t col_h);
+
+// Sets all LEDs in a rectangle given by vertices ([row_l], [col_l]) and
+// ([row_h], [col_h]) to the value of [bit].
+void fill_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
+               const int8_t row_h, const int8_t col_h, const bool bit);
+
+// Inverts all LEDs in a rectangle given by vertices ([row_l], [col_l]) and
+// ([row_h], [col_h]).
+void invert_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
+                 const int8_t row_h, const int8_t col_h);
+
+// Prints [msg] of length [msg_len] inside of a text box between [col_l] and
+// [col_h], with [row] being the lowest row and [spacing] being the amount
+// of empty space between each symbol. [step] shifts the text inside of the
+// text box to the right (step<0) and to the left (step>0).
+void draw_text_3_4(uint32_t f[], const bool symbols[][12], int8_t col_h,
+                   int8_t col_l, const int8_t row, const uint8_t msg[],
+                   const std::size_t msg_len, const uint8_t spacing,
+                   const int8_t step);
+
 // 3-by-5 digits
 /*
  |  #  |  #  | ##  | ### | # # | ### | ### | ### | ### | ### |
@@ -48,45 +87,5 @@ extern const bool DIGITS_35[10][15];
  |   # | ### | ### |  #  | ### | ### |
 */
 extern const bool DEFAULT_FONT_3_4[36][12];
-
-// Sets the LED at ([row], [col]) to the value of bit.
-void set_bit(uint32_t f[], const int8_t row, const int8_t col,
-                 const bool bit);
-
-// Inverts the state of the LED at ([row], [col]).
-void invert_bit(uint32_t f[], const int8_t row, const int8_t col);
-
-// Draws a [width]-by-[height] symbol to the LED matrix with its bottom
-// right corner at coordinates given by [row] and [column]. All LEDs
-// used by the symbol must lie within the matrix.
-void put_sym(uint32_t f[], const bool *symbols, const int8_t row,
-                 const int8_t col, const int8_t width, const int8_t height);
-
-// Works like put_sym() but skips every position that is outside of the
-// rectangle given by vertices ([row_l], [col_l]) and ([row_h], [col_h]).
-void put_sym_bnd(uint32_t f[], const bool *symbols, const int8_t row,
-                     const int8_t col, const int8_t width, const int8_t height,
-                     const int8_t row_l, const int8_t col_l, const int8_t row_h,
-                     const int8_t col_h);
-
-// Sets all LEDs in a rectangle given by vertices ([row_l], [col_l]) and
-// ([row_h], [col_h]) to the value of [bit].
-void fill_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
-                   const int8_t row_h, const int8_t col_h, const bool bit);
-
-// Inverts all LEDs in a rectangle given by vertices ([row_l], [col_l]) and
-// ([row_h], [col_h]).
-void invert_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
-                     const int8_t row_h, const int8_t col_h);
-
-// Prints [msg] of length [msg_len] inside of a text box between [col_l] and
-// [col_h], with [row] being the lowest row and [spacing] being the amount
-// of empty space between each symbol. [step] shifts the text inside of the
-// text box to the right (step<0) and to the left (step>0).
-void draw_text_3_4(uint32_t f[], const bool symbols[][12], int8_t col_h,
-                     int8_t col_l, const int8_t row, const uint8_t msg[],
-                     const std::size_t msg_len, const uint8_t spacing,
-                     const int8_t step);
-
 } // namespace LMG
 #endif
