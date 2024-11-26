248d1e1ba864f3b6e7f080fa9ea940ddee246c9c
msharipov <msharipovr@gmail.com>
2024-11-26 16:28:13 -0500

    refactor: use LMG namespace

--- LED_Matrix_Graphics.cpp
+++ LED_Matrix_Graphics.cpp
@@ -1,67 +1,64 @@
 #include "LED_Matrix_Graphics.h"
 #include <stdint.h>
 
-const bool DIGITS_35[10][15] = {
-  {0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0},
-  {1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0},
-  {1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1},
-  {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1},
-  {1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1},
-  {1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1},
-  {1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1},
-  {0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1},
-  {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
-  {0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1}
+namespace LMG {
+
+const bool DIGITS_35[10][15] = {{0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0},
+                                {1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0},
+                                {1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1},
+                                {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+                                {1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1},
+                                {1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+                                {1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+                                {0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1},
+                                {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+                                {0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1}};
+
+const bool DEFAULT_FONT_3_4[36][12] = {
+    {1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0}, // A
+    {1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1}, // B
+    {1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0}, // C
+    {0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1}, // D
+    {1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0}, // E
+    {0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0}, // F
+    {1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0}, // G
+    {1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1}, // H
+    {1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1}, // I
+    {0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1}, // J
+    {1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1}, // K
+    {1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, // L
+    {1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1}, // M
+    {1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0}, // N
+    {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1}, // O
+    {0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1}, // P
+    {1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0}, // Q
+    {1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1}, // R
+    {1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1}, // S
+    {0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1}, // T
+    {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1}, // U
+    {0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1}, // V
+    {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0}, // W
+    {1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1}, // X
+    {0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1}, // Y
+    {1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1}, // Z
+    {0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0}, // 0
+    {1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0}, // 1
+    {1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1}, // 2
+    {1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1}, // 3
+    {1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1}, // 4
+    {1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1}, // 5
+    {1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1}, // 6
+    {0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1}, // 7
+    {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1}, // 8
+    {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1}  // 9
 };
 
-const bool SYMBOLS_34[36][12] = {
-  {1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0}, // A
-  {1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1}, // B
-  {1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0}, // C
-  {0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1}, // D
-  {1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0}, // E
-  {0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0}, // F
-  {1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0}, // G
-  {1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1}, // H
-  {1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1}, // I
-  {0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1}, // J
-  {1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1}, // K
-  {1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, // L
-  {1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1}, // M
-  {1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0}, // N
-  {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1}, // O
-  {0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1}, // P
-  {1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0}, // Q
-  {1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1}, // R
-  {1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1}, // S
-  {0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1}, // T
-  {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1}, // U
-  {0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1}, // V
-  {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0}, // W
-  {1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1}, // X
-  {0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1}, // Y
-  {1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1}, // Z
-  {0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0}, // 0
-  {1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0}, // 1
-  {1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1}, // 2
-  {1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1}, // 3
-  {1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1}, // 4
-  {1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1}, // 5
-  {1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1}, // 6
-  {0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1}, // 7
-  {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1}, // 8
-  {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1}  // 9
-};
-
-
-void LMG_set_bit(uint32_t f[],
-                 const int8_t row,
-                 const int8_t col,
+void set_bit(uint32_t f[], const int8_t row, const int8_t col,
                  const bool bit) {
-  
+
   uint8_t q, r, pos;
-  
-  pos = row*12 + col;
+
+  pos = row * 12 + col;
 
   if (pos < 32) {
     q = 2;
@@ -71,7 +68,7 @@ void LMG_set_bit(uint32_t f[],
     q = 0;
   }
   r = pos % 32;
-  
+
   if (bit) {
     f[q] |= (1UL << r);
   } else {
@@ -79,14 +76,11 @@ void LMG_set_bit(uint32_t f[],
   }
 }
 
-
-void LMG_invert_bit(uint32_t f[],
-                    const int8_t row,
-                    const int8_t col) {
+void invert_bit(uint32_t f[], const int8_t row, const int8_t col) {
 
   uint8_t q, r, pos;
-  
-  pos = row*12 + col;
+
+  pos = row * 12 + col;
 
   if (pos < 32) {
     q = 2;
@@ -96,37 +90,25 @@ void LMG_invert_bit(uint32_t f[],
     q = 0;
   }
   r = pos % 32;
-  
+
   f[q] ^= (1UL << r);
 }
 
+void put_sym(uint32_t f[], const bool *symbol, const int8_t row,
+                 const int8_t col, const int8_t width, const int8_t height) {
 
-void LMG_put_sym(uint32_t f[],
-                 const bool * symbol,
-                 const int8_t row,
-                 const int8_t col,
-                 const int8_t width,
-                 const int8_t height) {
-  
   for (int8_t add_col = 0; add_col < width; add_col++) {
     for (int8_t add_row = 0; add_row < height; add_row++) {
-      
-      LMG_set_bit(f, row + add_row, col + add_col, 
-                     *(symbol + sizeof(bool)*(add_col + add_row*width)));
+
+      set_bit(f, row + add_row, col + add_col,
+                  *(symbol + sizeof(bool) * (add_col + add_row * width)));
     }
   }
 }
 
-
-void LMG_put_sym_bnd(uint32_t f[],
-                     const bool * symbol,
-                     const int8_t row,
-                     const int8_t col,
-                     const int8_t width,
-                     const int8_t height,
-                     const int8_t row_l,
-                     const int8_t col_l,
-                     const int8_t row_h,
+void put_sym_bnd(uint32_t f[], const bool *symbol, const int8_t row,
+                     const int8_t col, const int8_t width, const int8_t height,
+                     const int8_t row_l, const int8_t col_l, const int8_t row_h,
                      const int8_t col_h) {
 
   for (int8_t add_col = 0; add_col < width; add_col++) {
@@ -140,53 +122,37 @@ void LMG_put_sym_bnd(uint32_t f[],
       if (row + add_row > row_h || row + add_row < row_l) {
         continue;
       }
-      
-      LMG_set_bit(f, row + add_row, col + add_col, 
-                     *(symbol + sizeof(bool)*(add_col + add_row*width)));
+
+      set_bit(f, row + add_row, col + add_col,
+                  *(symbol + sizeof(bool) * (add_col + add_row * width)));
     }
   }
 }
 
-
-
-void LMG_fill_rect(uint32_t f[],
-                   const int8_t row_l,
-                   const int8_t col_l,
-                   const int8_t row_h,
-                   const int8_t col_h,
-                   const bool bit) {
+void fill_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
+                   const int8_t row_h, const int8_t col_h, const bool bit) {
 
   for (int8_t col = col_l; col <= col_h; col++) {
     for (int8_t row = row_l; row <= row_h; row++) {
-      LMG_set_bit(f, row, col, bit);
+      set_bit(f, row, col, bit);
     }
   }
 }
 
-
-void LMG_invert_rect(uint32_t f[],
-                   const int8_t row_l,
-                   const int8_t col_l,
-                   const int8_t row_h,
-                   const int8_t col_h) {
+void invert_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
+                     const int8_t row_h, const int8_t col_h) {
 
   for (int8_t col = col_l; col <= col_h; col++) {
     for (int8_t row = row_l; row <= row_h; row++) {
-      LMG_invert_bit(f, row, col);
+      invert_bit(f, row, col);
     }
   }
 }
 
-
-void LMG_put_text_34(uint32_t f[],
-                 const bool symbols[][12],
-                 int8_t col_h,
-                 int8_t col_l,
-                 const int8_t row,
-                 const uint8_t msg[], 
-                 const std::size_t msg_len,
-                 const uint8_t spacing,
-                 const int8_t step) {
+void draw_text_3_4(uint32_t f[], const bool symbols[][12], int8_t col_h,
+                     int8_t col_l, const int8_t row, const uint8_t msg[],
+                     const std::size_t msg_len, const uint8_t spacing,
+                     const int8_t step) {
 
   col_l = (col_l < 0) ? 0 : col_l;
   col_h = (col_h > 11) ? 11 : col_h;
@@ -196,14 +162,14 @@ void LMG_put_text_34(uint32_t f[],
 
   // Skip the printing if the text is completely out of bounds.
   // Warning: Removing the type cast causes incorrect integer promotion!
-  if (step >= (int16_t)(msg_len*block_width - spacing)) {
-    LMG_fill_rect(f, row, col_l, row + 3, col_h, 0);
+  if (step >= (int16_t)(msg_len * block_width - spacing)) {
+    fill_rect(f, row, col_l, row + 3, col_h, 0);
     return;
   }
 
   // Left-side spacing
   if (step < 0) {
-    LMG_fill_rect(f, row, col + 3, row + 3, col_h, 0);
+    fill_rect(f, row, col + 3, row + 3, col_h, 0);
   }
 
   for (int8_t sym = 0; sym < msg_len; sym++) {
@@ -219,12 +185,12 @@ void LMG_put_text_34(uint32_t f[],
 
     // Tail spacing before first symbol
     if (spacing != 0 && col_h - col >= 2 && col_h - col < block_width) {
-      LMG_fill_rect(f, row, col + 3, row + 3, col_h, 0);
+      fill_rect(f, row, col + 3, row + 3, col_h, 0);
     }
 
-    LMG_put_sym_bnd(f, symbols[msg[sym]], row, col, 3, 4,
-                    row, col_l, row + 3, col_h);
-    
+    put_sym_bnd(f, symbols[msg[sym]], row, col, 3, 4, row, col_l, row + 3,
+                    col_h);
+
     // Tail spacing
     if (spacing != 0 && col > col_l) {
 
@@ -233,7 +199,7 @@ void LMG_put_text_34(uint32_t f[],
         spc_end = col_l;
       }
 
-      LMG_fill_rect(f, row, spc_end, row + 3, col - 1, 0);
+      fill_rect(f, row, spc_end, row + 3, col - 1, 0);
     }
 
     col -= block_width;
@@ -241,6 +207,8 @@ void LMG_put_text_34(uint32_t f[],
 
   // Right-side space
   if (col > col_l) {
-    LMG_fill_rect(f, row, col_l, row + 3, col - 1, 0);
+    fill_rect(f, row, col_l, row + 3, col - 1, 0);
   }
 }
+
+} // namespace LMG
--- LED_Matrix_Graphics.h
+++ LED_Matrix_Graphics.h
@@ -1,9 +1,10 @@
 #ifndef MATRIX_PRINTING_H
 #define MATRIX_PRINTING_H
 
-#include <stdint.h>
 #include <cstddef>
+#include <stdint.h>
 
+namespace LMG {
 // 3-by-5 digits
 /*
  |  #  |  #  | ##  | ### | # # | ### | ### | ### | ### | ### |
@@ -46,71 +47,46 @@ extern const bool DIGITS_35[10][15];
  | ### |   # | ### |  #  | # # |   # |
  |   # | ### | ### |  #  | ### | ### |
 */
-extern const bool SYMBOLS_34[36][12];
+extern const bool DEFAULT_FONT_3_4[36][12];
 
 // Sets the LED at ([row], [col]) to the value of bit.
-void LMG_set_bit(uint32_t f[],
-                    const int8_t row,
-                    const int8_t col,
-                    const bool bit);
+void set_bit(uint32_t f[], const int8_t row, const int8_t col,
+                 const bool bit);
 
 // Inverts the state of the LED at ([row], [col]).
-void LMG_invert_bit(uint32_t f[],
-                    const int8_t row,
-                    const int8_t col);
+void invert_bit(uint32_t f[], const int8_t row, const int8_t col);
 
 // Draws a [width]-by-[height] symbol to the LED matrix with its bottom
 // right corner at coordinates given by [row] and [column]. All LEDs
 // used by the symbol must lie within the matrix.
-void LMG_put_sym(uint32_t f[],
-                 const bool * symbols,
-                 const int8_t row,
-                 const int8_t col,
-                 const int8_t width,
-                 const int8_t height);
+void put_sym(uint32_t f[], const bool *symbols, const int8_t row,
+                 const int8_t col, const int8_t width, const int8_t height);
 
-// Works like LMG_put_sym() but skips every position that is outside of the
+// Works like put_sym() but skips every position that is outside of the
 // rectangle given by vertices ([row_l], [col_l]) and ([row_h], [col_h]).
-void LMG_put_sym_bnd(uint32_t f[],
-                     const bool * symbols,
-                     const int8_t row,
-                     const int8_t col,
-                     const int8_t width,
-                     const int8_t height,
-                     const int8_t row_l,
-                     const int8_t col_l,
-                     const int8_t row_h,
+void put_sym_bnd(uint32_t f[], const bool *symbols, const int8_t row,
+                     const int8_t col, const int8_t width, const int8_t height,
+                     const int8_t row_l, const int8_t col_l, const int8_t row_h,
                      const int8_t col_h);
 
 // Sets all LEDs in a rectangle given by vertices ([row_l], [col_l]) and
 // ([row_h], [col_h]) to the value of [bit].
-void LMG_fill_rect(uint32_t f[],
-                   const int8_t row_l,
-                   const int8_t col_l,
-                   const int8_t row_h,
-                   const int8_t col_h,
-                   const bool bit);
+void fill_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
+                   const int8_t row_h, const int8_t col_h, const bool bit);
 
 // Inverts all LEDs in a rectangle given by vertices ([row_l], [col_l]) and
 // ([row_h], [col_h]).
-void LMG_invert_rect(uint32_t f[],
-                   const int8_t row_l,
-                   const int8_t col_l,
-                   const int8_t row_h,
-                   const int8_t col_h);
+void invert_rect(uint32_t f[], const int8_t row_l, const int8_t col_l,
+                     const int8_t row_h, const int8_t col_h);
 
 // Prints [msg] of length [msg_len] inside of a text box between [col_l] and
 // [col_h], with [row] being the lowest row and [spacing] being the amount
 // of empty space between each symbol. [step] shifts the text inside of the
 // text box to the right (step<0) and to the left (step>0).
-void LMG_put_text_34(uint32_t f[],
-                 const bool symbols[][12],
-                 int8_t col_h,
-                 int8_t col_l,
-                 const int8_t row,
-                 const uint8_t msg[],
-                 const std::size_t msg_len,
-                 const uint8_t spacing,
-                 const int8_t step);
+void draw_text_3_4(uint32_t f[], const bool symbols[][12], int8_t col_h,
+                     int8_t col_l, const int8_t row, const uint8_t msg[],
+                     const std::size_t msg_len, const uint8_t spacing,
+                     const int8_t step);
 
+} // namespace LMG
 #endif
