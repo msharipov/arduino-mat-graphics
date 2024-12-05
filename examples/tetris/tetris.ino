f27f28a3facee33641cda06966d44d10eb9c8b6d
msharipov <msharipovr@gmail.com>
2024-12-05 01:20:26 -0500

    feat(tetris): add Piece and GameState classes

---
+++ examples/tetris/tetris.ino
@@ -0,0 +1,45 @@
+/*!
+ *  Copyright 2024 Maxim Sharipov (msharipovr@gmail.com).
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
+#include <LED_Matrix_Graphics.h>
+#include "Arduino_LED_Matrix.h"
+#include <array>
+
+ArduinoLEDMatrix matrix{};
+LMG::Frame frame{};
+
+/// Represents a Tetris piece on the screen
+class Piece {
+
+};
+
+class GameState {
+  /// Holds the state of the pieces that have already been placed.
+  std::array<bool, LMG::LED_MATRIX_HEIGHT * LMG::LED_MATRIX_WIDTH> placed_pieces{};
+};
+
+void setup() {
+  matrix.begin();
+}
+
+void loop() {
+
+}
