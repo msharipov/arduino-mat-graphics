321035710b47a90e0a46acf4d26d4550fa54e37b
msharipov <msharipovr@gmail.com>
2025-01-04 19:02:26 -0500

    docs(volt_diff): add comments

--- examples/volt_diff/volt_diff.ino
+++ examples/volt_diff/volt_diff.ino
@@ -22,8 +22,8 @@
 
 /* This program allows you to quickly measure voltage using the Arduino. The
  * program will display the potential difference between pins A0 and A1 as a
- * fraction of the difference between 5V and GND on the LED matrix. If the
- * difference happens to be negative, a minus sign will also be displayed.
+ * fraction of the difference between 5V and GND. If the difference happens to
+ * be negative, a negative sign will also be displayed.
  */
 
 #include "Arduino_LED_Matrix.h"
@@ -31,7 +31,6 @@
 #include <cstdint>
 
 ArduinoLEDMatrix matrix{};
-LMG::Frame frame{};
 
 void setup() {
   matrix.begin();
@@ -41,11 +40,19 @@ void setup() {
 
 void loop() {
   using LMG::Rect;
+
+  // Index at which the sprites for digits start in the default font.
   const size_t DIGITS_OFFSET{29};
+
+  // Areas where the digits are drawn.
   const Rect FIRST_DIGIT{0, 4, 1, 3};
   const Rect SECOND_DIGIT{0, 4, 5, 7};
   const Rect THIRD_DIGIT{0, 4, 9, 11};
+
+  // The 1 in 1.00 has to be displayed differently.
   const Rect FIRST_DIGIT_ALTERNATE{0, 4, 0, 2};
+
+  // Area where the negative sign is drawn.
   const Rect NEGATIVE_SIGN{6, 6, 8, 10};
 
   int16_t rel_voltage = analogRead(A0) - analogRead(A1);
@@ -55,34 +62,34 @@ void loop() {
     rel_voltage = -rel_voltage;
   }
 
+  // Determines the digits of the fraction.
   const uint8_t first = rel_voltage * 10 / 1023;
   const uint8_t second = rel_voltage * 100 / 1023 % 10;
   const uint8_t third = rel_voltage * 1000 / 1023 % 10;
 
-  frame.fillRect(
-      Rect(0, LMG::LED_MATRIX_HEIGHT - 1, 0, LMG::LED_MATRIX_WIDTH - 1), LOW);
+  LMG::Frame frame{};
 
   if (rel_voltage == 1023) {
-
+    // Prints 1.00 if there is no volage difference.
     frame.drawSprite(LMG::DEFAULT_FONT_3x5[DIGITS_OFFSET + 1],
                      FIRST_DIGIT_ALTERNATE);
-    frame.setLED(4, 3, HIGH);
+    frame.setLED(4, 3, HIGH); // Decimal point
     frame.drawSprite(LMG::DEFAULT_FONT_3x5[DIGITS_OFFSET], SECOND_DIGIT);
     frame.drawSprite(LMG::DEFAULT_FONT_3x5[DIGITS_OFFSET], THIRD_DIGIT);
-
   } else {
-
-    frame.setLED(4, 0, HIGH);
+    frame.setLED(4, 0, HIGH); // Decimal point
     frame.drawSprite(LMG::DEFAULT_FONT_3x5[DIGITS_OFFSET + first], FIRST_DIGIT);
     frame.drawSprite(LMG::DEFAULT_FONT_3x5[DIGITS_OFFSET + second],
                      SECOND_DIGIT);
     frame.drawSprite(LMG::DEFAULT_FONT_3x5[DIGITS_OFFSET + third], THIRD_DIGIT);
   }
 
+  // Draws the negative sign.
   if (negative) {
     frame.fillRect(NEGATIVE_SIGN, HIGH);
   }
 
+  // Updates the matrix.
   matrix.loadFrame(frame.getData());
   delay(200);
 }
